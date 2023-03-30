b2cf416ad150bd2115ca57115858bbf5ec4b3665
Enoxya <enoxya+github@gmail.com>
2023-03-30 17:09:49 +0200

    Create PS_PowerCLI_VMs_Reseaux_Infos.ps1

---
+++ PowerShell/PowerCLI/PS_PowerCLI_VMs_Reseaux_Infos.ps1
@@ -0,0 +1,27 @@
+﻿#VDéfinition des variables
+$myVMHost = Get-VMHost "kiev.chb.ts1.local"
+
+##Hosts
+#Récupérer la liste des hôtes ESX d'un Cluster :
+    Get-VMHost
+
+##vSwitches
+#Récupérer les vSwitches d'un hôte ESX
+    Get-VMHost -Name $myVMHost | Get-VirtualSwitch
+
+#Récupérer les infos d'un seul vSwitch d'un hôte ESX
+    Get-VMHost -Name $myVMHost | Get-VirtualSwitch -Name vSwitch0
+
+#Autres
+    #Retrieve the virtual switch used by the virtual machine named VM :
+        Get-VirtualSwitch -VM VM
+    #Retrieve all virtual switches in the specified datacenter :
+        Get-Datacenter -Name "MyDatacenter" | Get-VirtualSwitch
+    #Retrieves all virtual switches named "vSwitch0" :
+        Get-VirtualSwitch -Name "vSwitch0"
+
+
+##vmnic
+$myVMHostNetworkAdapter = Get-VMhost $myVMHost | Get-VMHostNetworkAdapter -Physical -Name vmnic2
+#Récupérer la liste des adaptateurs physiques d'un hôte ESX :
+    Get-VMhost $myVMHost | Get-VMHostNetworkAdapter -Physical
