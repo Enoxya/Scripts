74738f917a16d005d2f0eb6eac8427931274824c
Enoxya <enoxya+github@gmail.com>
2023-04-07 23:07:58 +0200

    Nettoyage

--- PowerShell/Citrix/Administration/Remove-desktop-ini.ps1
+++
@@ -1,6 +0,0 @@
-$arrUsers = Get-ChildItem ("\\srv-ctx-file\Users$\Documents") | Where-Object {$_.PSIsContainer} | Foreach-Object {$_.FullName}
-Foreach ($strUser in $arrUsers) {
-	$ErrorActionPreference = "SilentlyContinue"
-	$strFile = $strUser + "\desktop.ini"
-	Remove-Item -Path $strFile -Force
-}
--- PowerShell/Citrix/Administration/gpupdate -force-Renault.ps1
+++
@@ -1,9 +0,0 @@
-$List = Get-Content "C:\_admin\Scripts\ListeXenApp-Renault.txt"
-
-foreach ($Server in $List)
-{
-
-Write-Host "Gpupdate sur serveur " $Server
-invoke-command -ScriptBlock {gpupdate /force} -ComputerName $Server
-
-}
